6b8b3d37cb3392590212e212d50ca087dd8065e1
Christian Elliott <cdelliott96@gmail.com>
2024-05-22 00:50:19 -0500

    Update Items fixtures and rubocop -A

--- db/migrate/20240522052050_update_items_nullables.rb
+++ db/migrate/20240522052050_update_items_nullables.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class UpdateItemsNullables < ActiveRecord::Migration[7.1] # rubocop:disable Style/Documentation
   def change
     change_column_null :items, :hn_id, false
